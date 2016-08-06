70ec1d32db7bf10a15f9281694a3eabba6849989
Gentry Rolofson <xdarkicex@gmail.com>
2016-08-06 13:48:18 -0700

    fixing things add return

--- jquery.highlighter.js
+++ jquery.highlighter.js
@@ -1,9 +1,10 @@
 (function($)){
   $.fn.highLighter = function(){
-    $('body').click(function(e){
+    this.click(function(e){
       var obj = $(e.target.parentNode);
       if(obj.data('href')) window.document.location = obj.data('href');
       if(obj.data('href-new')) window.open(obj.data('href-new'));
     });
+    return this;
   };
 }(jquery));
